f9822c6a5bcc37e55b946627c936870d74bc4a84
MariaMacedo-Prog <mariamacedo2xx@gmail.com>
2024-03-23 13:42:34 -0300

    finishing project card and page

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-card.component.css']
 })
 export class ProjectCardComponent implements OnInit {
+  @Input() dataInfo: {image: string, name: string,description: string,color: string} = {
+    image: "nochat_container.png",
+    name: "No Chat",
+    description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+    color: "#00C2FF"
+  };
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-
+  listProjects=[
+    {
+      image: "nochat_container.png",
+      name: "No Chat",
+      description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+      color: "#00C2FF"
+    },
+    {
+      image: "caris_container.png",
+      name: "Caris",
+      description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+      color: "#00ff95"
+    },
+    {
+      image: "oliveira_container.png",
+      name: "Oliveira",
+      description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+      color: "#f87ae7"
+    },
+    {
+      image: "olx_container.png",
+      name: "OLX",
+      description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+      color: "#f87a7a"
+    },
+    {
+      image: "mario_container.png",
+      name: "Jogo Mario",
+      description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+      color: "#ff970e"
+    },
+    {
+      image: "jogo_tiro_container.png",
+      name: "Jogo Mario",
+      description: "Uma rede social, bla bla bal, Uma rede social, bla bla bal, Uma rede social, bla bla bal, ",
+      color: "#ff85"
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+  visibleCards = 2;
+  // Detecta o evento de rolagem
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight;
+    const max = document.documentElement.scrollHeight;
+    
+    // Se o usuário rolar para baixo e estiver perto da metade da tela
+    if (pos >= max / 2) {
+      this.loadMore(); // Carrega mais cartões
+    }
+  }
+ 
+  // Função para carregar mais cartões
+  loadMore(): void {
+    this.visibleCards +=1; // Adicione o número de cartões que deseja carregar a cada vez
   }
 
 }
